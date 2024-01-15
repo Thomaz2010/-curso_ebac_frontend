4eeed505da63b7a8a47f695a9b7af48cecd08dd2
Thomaz Simão <thomazsimao5@gmail.com>
2024-01-15 16:02:40 -0300

    tarefa

--- main.js
+++ main.js
@@ -1,36 +1,32 @@
-function validaForms() {
- const campoA = document.getElementById("campoA").value;
- const campoB = document.getElementById("campoB").value;
+$(document).ready(function () {
+    $('header button').click(function () {
+        $('form').slideDown();
+    })
 
-  function exibirmsg(valido, mensagem) {
-    let mesgDiv = document.getElementById("mensagem");
-    mesgDiv.innerHTML = mensagem;
-  
-    if (valido) {
-      mesgDiv.style.color = 'green';
-    } else {
-      mesgDiv.style.color = "red";
-    }
-  }
 
-  if (campoB > campoA) {
-    exibirmsg(true, "***Formulário Válido***");
-    limpaNome();
-  } else {
-    exibirmsg(false,  "***Formulário Inválido***");
-    limpaNome();
-  }
+    $('#btn-cancelar').click(function () {
+        $('form').slideUp();
+    })
 
-  if ((campoA == "") & (campoB == "")) {
-    alert("Preencha os Dados Corretamente");
-    exibirmsg(false, "Preencha os Dados Corretamente");
-    limpaNome();
-  }
-}
+    $('form').on('submit', function (e) {
+        e.preventDefault();
+        const novatarefa = $("#endereço-tarefa").val();
+        //console.log(novatarefa)
+
+        const novoItem = $(`<li>${novatarefa}</li>`);
+        $(`${novatarefa}`).appendTo(novoItem);
+        $(novoItem).appendTo("ul");
+        $("#endereço-tarefa").val('');
+
+    })
+
+    $("ul").on("click", "li", function () {
+        $(this).toggleClass("tConcluido");
+    });
+})
 
 
 
-function limpaNome() {
-  campoA.value = "";
-  campoB.value = "";
-}
+
+
+
