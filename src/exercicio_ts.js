2a3a3eee253d70027b206fea38bb19f8427248a8
Thomaz Simão <thomazsimao5@gmail.com>
2024-08-15 11:40:25 -0300

    commit inicial

---
+++ src/exercicio_ts.js
@@ -0,0 +1,8 @@
+function multPlica(number1, number2) {
+    return number1 * number2;
+}
+console.log(multPlica(2, 5));
+function fraseInicial(nome) {
+    return nome;
+}
+console.log(fraseInicial('Olá' + ' ' + 'Thomaz'));
---
+++ src/exercicio_ts.ts
@@ -0,0 +1,10 @@
+function multPlica(number1:number , number2:number):number {
+    return number1 * number2
+}
+console.log(multPlica(2,5));
+
+function fraseInicial(nome:string):string {
+    return nome
+
+}
+console.log(fraseInicial('Olá'  + ' ' +  'Thomaz'));
